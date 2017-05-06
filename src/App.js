d5bbc4a925db8b9ed4ca434d41fadca512435b3c
Raj <rkatta@mail.bradley.edu>
2017-05-06 15:44:21 -0500

    varibles

--- src/App.js
+++ src/App.js
@@ -6,18 +6,35 @@ import Row from './row'
 
 class App extends Component {
 
-  render() {
+  render()
+  {
+      var a=[];
+
+      var i=0;
+      for(;i<9; i++)
+      {
+          a[i]=i+1;
+      }
+      function ready(){
+
+
+
+      }
+      function handlechange() {
+          a[0]="x";
+
+      }
     return (
         <div>
 
-        <button class="btn" id="reset">reset</button>
+        <button class="btn" onclick={ready} id="reset">reset</button>
             <div className="container title">
                 Tic Tac Toe</div>
             <div className="container description">Two Human Players - X Moves First</div>
             <div className="container text-center">
-            <Row className="row" numb="1" numb1="2" numb2="3"/>
-            <Row className="row" numb="4" numb1="5" numb2="6"/>
-            <Row className="row" numb="7" numb1="8" numb2="9"/>
+            <Row className="row" onchange={handlechange} numb={a[0]} numb1={a[1]} numb2={a[2]}/>
+            <Row className="row" onchange={handlechange} numb={a[3]} numb1={a[4]} numb2={a[5]}/>
+            <Row className="row" onchange={handlechange} numb={a[6]} numb1={a[7]} numb2={a[8]}/>
             </div>
         </div>
 
--- src/square.js
+++ src/square.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './square.css';
 class Square extends Component {
+
     render() {
+
         return (
             <div className="square">{this.props.number}</div>
 
