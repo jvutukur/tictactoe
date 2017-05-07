1e603ee69b5900d4fa7f8303c95c26ec042dabba
jvutukur <jvutukur@asu.edu>
2017-05-07 10:25:23 -0700

    handled game complete state

--- src/App.js
+++ src/App.js
@@ -1,103 +1,98 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Row from './row'
 
 
 class App extends Component {
 
-    constructor()
-    {
+    constructor() {
         super();
         this.state = {
-          squares : Array(9).fill(null),
-            s:Array(9).fill(null),
-          currentSymbol : 'X',
-            previousSymbol : '0',
+            squares: Array(9).fill(null),
+            s: Array(9).fill(null),
+            currentSymbol: 'X',
+            winner: null,
         };
-        for(var i=0; i<9; i++){
-            this.state.s[i]=i+1;
-            this.state.squares[i]=i+1;//squares.
+        for (var i = 0; i < 9; i++) {
+            this.state.s[i] = i + 1;
+            this.state.squares[i] = i + 1;//squares.
         }
     };
 
-     handleClick(i){
+    handleClick(i) {
+        if (i == 'X' || i == 'O' || this.state.winner)
+            return;
+        else {
+            const squares = this.state.squares.slice();
+            this.state.previousSymbol = this.state.currentSymbol;
+            squares[i - 1] = this.state.currentSymbol;
+            this.setState({squares: squares});
+            this.toggleCurrentSymbol();
+        }
+    };
 
-         if(i=='X' || i=='O'){
-                return;
+
+    calculateWinner() {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (this.state.squares[a] && this.state.squares[a] === this.state.squares[b] && this.state.squares[a] === this.state.squares[c]) {
+                this.state.winner = this.state.squares[a];
             }
-            else{
-                 const squares = this.state.squares.slice();
-             this.state.previousSymbol=this.state.currentSymbol;
-                 squares[i-1] = this.state.currentSymbol;
-                 this.setState({squares: squares});
-                 this.toggleCurrentSymbol();
-            }
-
-        };
-
-
-    calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
         }
     }
-    return null;
-}
-     toggleCurrentSymbol(){
-         if(this.state.currentSymbol == 'X')
-             this.setState({currentSymbol : 'O'});
-         else
-             this.setState({currentSymbol : 'X'});
-     };
-    restart(){
 
-        this.reset();
+    toggleCurrentSymbol() {
+        if (this.state.currentSymbol == 'X')
+            this.setState({currentSymbol: 'O'});
+        else
+            this.setState({currentSymbol: 'X'});
+    };
 
+    restart() {
+        this.setState({squares: this.state.s,
+              winner : null,
+              currentSymbol : 'X',
+        });
     }
+
     render() {
-    var status = 'Next Player : '+ 'X';
-        const winner = this.calculateWinner(this.state.squares);
+        this.calculateWinner(this.state.squares);
+        var status;
+        if (this.state.winner)
+            status = 'Winner: ' + this.state.winner;
+        else
+            status = 'Next Player : ' + this.state.currentSymbol;
+        return (
+            <div>
+                <button className="btn" id="reset" onClick={()=> this.restart()}>reset</button>
+                <div className="container1 title">
+                    Tic Tac Toe
+                </div>
 
-        if (winner) {
-            status = 'Winner: ' + this.state.previousSymbol;
-
-
-
-        } else {
-            status = 'Next player: ' + this.state.currentSymbol;
-        }
-    return (
-
-
-        <div>
-
-        <button className="btn"  id="reset" onClick={()=> this.setState({squares: this.state.s})}>reset</button>
-            <div className="container1 title">
-                   Tic Tac Toe</div>
-
-            <div className="container text-center">
-            <Row className="row"  numb={this.state.squares[0]} numb1={this.state.squares[1]} numb2={this.state.squares[2]} appClick={(i) =>this.handleClick(i)}/>
-            <Row className="row"  numb={this.state.squares[3]} numb1={this.state.squares[4]} numb2={this.state.squares[5]} appClick={(i) => this.handleClick(i)}/>
-            <Row className="row"  numb={this.state.squares[6]} numb1={this.state.squares[7]} numb2={this.state.squares[8]} appClick={(i) => this.handleClick(i)}/>
+                <div className="container text-center">
+                    <Row className="row" numb={this.state.squares[0]} numb1={this.state.squares[1]}
+                         numb2={this.state.squares[2]} appClick={(i) =>this.handleClick(i)}/>
+                    <Row className="row" numb={this.state.squares[3]} numb1={this.state.squares[4]}
+                         numb2={this.state.squares[5]} appClick={(i) => this.handleClick(i)}/>
+                    <Row className="row" numb={this.state.squares[6]} numb1={this.state.squares[7]}
+                         numb2={this.state.squares[8]} appClick={(i) => this.handleClick(i)}/>
+                </div>
+                <div className="container3 status">{status} </div>
             </div>
-            <div className="container3 status">{status} </div>
-        </div>
 
-            );
+        );
 
-  }
+    }
 
 }
 
